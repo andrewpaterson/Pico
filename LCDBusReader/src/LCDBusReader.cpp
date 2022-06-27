2fdeb8a0edeb0c421154d255a4b6aef50a776633
Andrew Paterson <andrew.ian.paterson@gmail.com>
2022-06-27 21:26:12 +0200

    Fixed shift register and ran out of disk space.

--- LCDBusReader/src/LCDBusReader.cpp
+++ LCDBusReader/src/LCDBusReader.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "pico/binary_info.h"
 #include "LCDDisplay.h"
+#include "ShiftRegister.h"
 #include "GeneralPins.h"
 
 
@@ -67,33 +67,24 @@ uint make_led_mask(uint val)
 
 int main() 
 {
-    bi_decl(bi_program_description("LCD Display."));
-
     stdio_init_all();
+
+    S595OutPins sShiftOut;
+    sShiftOut.Init(PIN_NOT_SET, 18, 17, 16, PIN_NOT_SET);
+    init_shift(&sShiftOut);
 
     S11BitLCDPins s11BitPins;
 
     s11BitPins.Init(13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3);
-
     init_lcd(&s11BitPins);
+    gpio_init(17);
+    gpio_set_dir(17, true);
 
-    uint latchMask = make_latch_mask(0xff);
-    gpio_init_mask(latchMask);
-    gpio_set_dir_in_masked(latchMask);
+    gpio_init(18);
+    gpio_set_dir(18, true);
 
-    uint chipSelectMask = make_chip_select_mask(0x7);
-    gpio_init_mask(chipSelectMask);
-    gpio_set_dir_out_masked(chipSelectMask);
-    gpio_put_masked(chipSelectMask, 0);
-
-    gpio_init(PIN_LE);
-    gpio_set_dir(PIN_LE, true);
-    gpio_put(PIN_LE, false);
-
-    uint ledMask = make_led_mask(0x7);
-    gpio_init_mask(ledMask);
-    gpio_set_dir_out_masked(ledMask);
-    gpio_put_masked(ledMask, 0);
+    gpio_init(19);
+    gpio_set_dir(19, true);
 
     bool led = true;
     gpio_init(PIN_LED_PI);
@@ -103,65 +94,75 @@ int main()
     char szLine2[16];
     char szDecimal[16];
 
-    uint chipTested = 0xff;
+    int iValue = 1;
     while(true)
     {
         gpio_put(PIN_LED_PI, led);
         
-        gpio_put(PIN_LE, true);
-        sleep_us_high_power(1);
-        gpio_put(PIN_LE, false);
+        // gpio_put(PIN_LE, true);
+        // sleep_us_high_power(1);
+        // gpio_put(PIN_LE, false);
 
-        strcpy(szLine1, " 00|01|02|03|04");
-        szLine2[0] = ' ';
-        szLine2[1] = '\0';
-        for (int chip = 0; chip <= 4; chip++)
-        {
-            if ((chip == chipTested) || (chipTested == 0xff))
-            {
-                uint chipPins = make_chip_select_mask(chip);
-                gpio_put_masked(chipSelectMask, chipPins);
-                sleep_us_high_power(1);
+        // gpio_put_masked(ledMask, led ? 7 : 0);
+
+        // strcpy(szLine1, " 00|01|02|03|04");
+        // szLine2[0] = ' ';
+        // szLine2[1] = '\0';
+        // for (int chip = 0; chip <= 4; chip++)
+        // {
+        //     if ((chip == chipTested) || (chipTested == 0xff))
+        //     {
+        //         uint chipPins = make_chip_select_mask(chip);
+        //         gpio_put_masked(chipSelectMask, chipPins);
+        //         sleep_us_high_power(1);
                 
-                uint v0 = gpio_get(PIN_L0);
-                uint v1 = gpio_get(PIN_L1) << 1;
-                uint v2 = gpio_get(PIN_L2) << 2;
-                uint v3 = gpio_get(PIN_L3) << 3;
-                uint v4 = gpio_get(PIN_L4) << 4;
-                uint v5 = gpio_get(PIN_L5) << 5;
-                uint v6 = gpio_get(PIN_L6) << 6;
-                uint v7 = gpio_get(PIN_L7) << 7;
+        //         uint v0 = gpio_get(PIN_L0);
+        //         uint v1 = gpio_get(PIN_L1) << 1;
+        //         uint v2 = gpio_get(PIN_L2) << 2;
+        //         uint v3 = gpio_get(PIN_L3) << 3;
+        //         uint v4 = gpio_get(PIN_L4) << 4;
+        //         uint v5 = gpio_get(PIN_L5) << 5;
+        //         uint v6 = gpio_get(PIN_L6) << 6;
+        //         uint v7 = gpio_get(PIN_L7) << 7;
 
-                uint val = (v0 | v1 | v2 | v3 | v4 | v5 | v6 | v7); // | (rand() & 255);
+        //         uint val = (v0 | v1 | v2 | v3 | v4 | v5 | v6 | v7); // | (rand() & 255);
 
-                if (val < 16)
-                {
-                    strcat(szLine2, "0");
-                    strcat(szLine2, itoa(val, szDecimal, 16));
-                }
-                else if (val < 256)
-                {
-                    strcat(szLine2, itoa(val, szDecimal, 16));
-                }
-                else
-                {
-                    strcat(szLine2, "XX");
-                }
-            }
-            else
-            {
-                strcat(szLine2, "  ");
-            }
+        //         if (val < 16)
+        //         {
+        //             strcat(szLine2, "0");
+        //             strcat(szLine2, itoa(val, szDecimal, 16));
+        //         }
+        //         else if (val < 256)
+        //         {
+        //             strcat(szLine2, itoa(val, szDecimal, 16));
+        //         }
+        //         else
+        //         {
+        //             strcat(szLine2, "XX");
+        //         }
+        //     }
+        //     else
+        //     {
+        //         strcat(szLine2, "  ");
+        //     }
 
-            if (chip != 4)
-            {
-                strcat(szLine2, "|");
-            }
+        //     if (chip != 4)
+        //     {
+        //         strcat(szLine2, "|");
+        //     }
+        // }
+
+        // put_clear(&s11BitPins);
+        // put_lines(&s11BitPins, szLine1, szLine2);
+
+        shift_out(&sShiftOut, (uint16_t)iValue);
+        iValue <<= 1;
+        if (iValue == 0x10000)
+        {
+            iValue = 1;
         }
 
-        put_clear(&s11BitPins);
-        put_lines(&s11BitPins, szLine1, szLine2);
-        sleep_us_high_power(100000);
+        sleep_us_high_power(20000);
 
         led = !led;
     }
