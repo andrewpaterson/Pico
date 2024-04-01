f50f7c7dcbaed712c2eb63d44e9ba3530d053e0d
Andrew Paterson <andrew.ian.paterson@gmail.com>
2024-04-01 10:09:45 +0200

    16Bit GPIO Outputs

--- LCDBusReader/Source/LCDBusReader.cpp
+++ LCDBusReader/Source/LCDBusReader.cpp
@@ -1048,24 +1048,166 @@ void key_pressed(char c)
 }
 
 
+#define READ_DATA_0__7      8
+#define READ_DATA_8__15     9
+#define READ_IN             10
+#define WRITE_OUT           11
+#define WRITE_DATA_0__7     12
+#define WRITE_DATA_8__15    13
+#define OUT_ENABLE_0__7     14
+#define OUT_ENABLE_8__15    15
+#define DATA_0              16
+#define DATA_1              17
+#define DATA_2              18
+#define DATA_3              19
+#define DATA_4              20
+#define DATA_5              21
+#define DATA_6              22
+#define DATA_7              26
+
+int aiData[] = { DATA_0, DATA_1, DATA_2, DATA_3, DATA_4, DATA_5, DATA_6, DATA_7 };
+
 
 int main() 
 {
-    init_io_and_led();
-    gpio_put(25, true);
+    // init_io_and_led();
+    // gpio_put(25, true);
 
 
-    init_sound(&sSound, 0, 1, 14, 15, 2, 3, 4, 5, 6,7, 8, 9);
+    // init_sound(&sSound, 0, 1, 14, 15, 2, 3, 4, 5, 6,7, 8, 9);
 
-    struct repeating_timer timer;
-    add_repeating_timer_us(-500, repeating_timer_callback, NULL, &timer);
+    // struct repeating_timer timer;
+    // add_repeating_timer_us(-500, repeating_timer_callback, NULL, &timer);
 
-    do_keypad_lcd();
+    //do_keypad_lcd();
     
     //do_uart_slave(0, 1, 115200);
     // busy_wait_us_32(250'000);
 
     // do_uart_master(0, 1, 115200);
-    gpio_put(25, false);
+    // gpio_put(25, false);
+
+    stdio_init_all();
+    gpio_init(25);
+    gpio_init(READ_DATA_0__7);
+    gpio_init(READ_DATA_8__15);
+    gpio_init(READ_IN);
+    gpio_init(WRITE_OUT);
+    gpio_init(WRITE_DATA_0__7);
+    gpio_init(WRITE_DATA_8__15);
+    gpio_init(OUT_ENABLE_0__7);
+    gpio_init(OUT_ENABLE_8__15);
+    gpio_init(DATA_0);
+    gpio_init(DATA_1);
+    gpio_init(DATA_2);
+    gpio_init(DATA_3);
+    gpio_init(DATA_4);
+    gpio_init(DATA_5);
+    gpio_init(DATA_6);
+    gpio_init(DATA_7);
+
+
+    gpio_put_all(0);
+
+    gpio_set_dir(25, GPIO_OUT);
+    gpio_set_dir(READ_DATA_0__7, GPIO_OUT);
+    gpio_set_dir(READ_DATA_8__15, GPIO_OUT);
+    gpio_set_dir(READ_IN, GPIO_OUT);
+    gpio_set_dir(WRITE_OUT, GPIO_OUT);
+    gpio_set_dir(WRITE_DATA_0__7, GPIO_OUT);
+    gpio_set_dir(WRITE_DATA_8__15, GPIO_OUT);
+    gpio_set_dir(OUT_ENABLE_0__7, GPIO_OUT);
+    gpio_set_dir(OUT_ENABLE_8__15, GPIO_OUT);
+    gpio_set_dir(DATA_0, GPIO_OUT);
+    gpio_set_dir(DATA_1, GPIO_OUT);
+    gpio_set_dir(DATA_2, GPIO_OUT);
+    gpio_set_dir(DATA_3, GPIO_OUT);
+    gpio_set_dir(DATA_4, GPIO_OUT);
+    gpio_set_dir(DATA_5, GPIO_OUT);
+    gpio_set_dir(DATA_6, GPIO_OUT);
+    gpio_set_dir(DATA_7, GPIO_OUT);
+
+    gpio_put(25, true);
+    gpio_put(DATA_0, true);
+    gpio_put(DATA_1, true);
+    gpio_put(DATA_2, true);
+    gpio_put(DATA_3, true);
+    gpio_put(DATA_4, true);
+    gpio_put(DATA_5, true);
+    gpio_put(DATA_6, true);
+    gpio_put(DATA_7, true);
+    gpio_put(OUT_ENABLE_0__7, true);
+    gpio_put(OUT_ENABLE_8__15, true);
+    gpio_put(WRITE_DATA_0__7, true);
+    gpio_put(WRITE_DATA_8__15, true);
+    gpio_put(WRITE_OUT, true);
+    sleep_ms(1);
+    gpio_put(OUT_ENABLE_0__7, false);
+    gpio_put(OUT_ENABLE_8__15, false);
+    gpio_put(WRITE_DATA_0__7, false);
+    gpio_put(WRITE_DATA_8__15, false);
+    gpio_put(WRITE_OUT, false);
+
+    int i = 0;
+    for (;;)
+    {
+        sleep_ms(32);
+        gpio_put(25, false);
+
+        gpio_put(DATA_0, false);
+        gpio_put(DATA_1, false);
+        gpio_put(DATA_2, false);
+        gpio_put(DATA_3, false);
+        gpio_put(DATA_4, false);
+        gpio_put(DATA_5, false);
+        gpio_put(DATA_6, false);
+        gpio_put(DATA_7, false);
+
+        if ((i >= 0) && (i <= 7))
+        {
+            gpio_put(WRITE_DATA_8__15, true);
+            gpio_put(WRITE_DATA_8__15, false);
+            gpio_put(aiData[7 - i], true);
+            gpio_put(WRITE_DATA_0__7, true);
+            gpio_put(WRITE_DATA_0__7, false);
+        }
+        else if ((i >= 8) && (i <= 15))
+        {
+            gpio_put(WRITE_DATA_0__7, true);
+            gpio_put(WRITE_DATA_0__7, false);
+            gpio_put(aiData[i - 8], true);
+            gpio_put(WRITE_DATA_8__15, true);
+            gpio_put(WRITE_DATA_8__15, false);
+        }
+        else if ((i >= 16) && (i <= 23))
+        {
+            gpio_put(WRITE_DATA_0__7, true);
+            gpio_put(WRITE_DATA_0__7, false);
+            gpio_put(aiData[23 - i], true);
+            gpio_put(WRITE_DATA_8__15, true);
+            gpio_put(WRITE_DATA_8__15, false);
+        }
+        else if ((i >= 24) && (i <= 31))
+        {
+            gpio_put(WRITE_DATA_8__15, true);
+            gpio_put(WRITE_DATA_8__15, false);
+            gpio_put(aiData[i - 24], true);
+            gpio_put(WRITE_DATA_0__7, true);
+            gpio_put(WRITE_DATA_0__7, false);
+        }
+
+        gpio_put(WRITE_OUT, true);
+        gpio_put(WRITE_OUT, false);
+
+        sleep_ms(32);
+        gpio_put(25, true);
+
+        i++;
+        if (i == 32)
+        {
+            i = 0;
+        }
+    }
+    return 0;
 }
 
